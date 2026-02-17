7e7eff969cc01c0a4b13fabdfcb3791eb3ee7774
Jimmy Christiaens <jimmy.christiaens@vodafoneziggo.com>
2026-02-17 09:14:34 +0100

    adding mvc controller

--- lab/36-mvc/src/main/java/accounts/web/AccountController.java
+++ lab/36-mvc/src/main/java/accounts/web/AccountController.java
@@ -3,6 +3,10 @@ package accounts.web;
 import accounts.AccountManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import rewards.internal.account.Account;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
@@ -18,7 +22,7 @@ import java.util.List;
  *
  */
 // TODO-03: Add an appropriate annotation to make this class a REST controller
-	
+@RestController
 public class AccountController {
 
 	private final AccountManager accountManager;
@@ -35,30 +39,18 @@ public class AccountController {
 	 * Return a list of all accounts
 	 */
 	// TODO-04: Add an appropriate annotation to make this method handle "/accounts"
-
+	@GetMapping("/accounts")
 	public List<Account> accountList() {
-
-		// TODO-05: Implement the logic to find and return all accounts
-		// - Use "accountManger" object to get all accounts
-		// - Recompile this class if necessary, and wait for the application to restart (via devtools)
-		// - From the home page, click the link - this should now work
-		// - If you prefer, access http://localhost:8080/accounts using curl or Postman
-
-		return null; // REPLACE THIS LINE to return a list accounts
-		
-		// TODO-06: (If you are using STS) We are about to make lots of
-		//          changes, so stop the application otherwise Devtools
-		//          will keep restarting it.
+		return accountManager.getAllAccounts();
 	}
 
-	// TODO-08: Implement the /accounts/{entityId} request handling method.
-	// - Call the method accountDetails().
-	// - Annotate to define URL mapping /accounts/{entityId}
-	//   this method will respond to.
-	// - Use a method parameter to obtain the URI template parameter
-	//   needed to retrieve an account.
-	// - Use the accountManager to obtain an account. This is the value to return
-	// - Save all work.
+	/**
+	 * Provide a model with an account for the account detail page.
+	 */
+	@GetMapping("/accounts/{entityId}")
+	public Account accountDetails(@PathVariable("entityId") long id) {
+		return accountManager.getAccount(id);
+	}
 
 
 	// TODO-10b: If AccountControllerTests.testHandleDetailsRequest()
