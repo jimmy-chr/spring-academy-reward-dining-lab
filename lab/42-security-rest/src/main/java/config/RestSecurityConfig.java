df2a152b871e0b45f38c5b1c8c75606f63a46051
Jimmy Christiaens <jimmy.christiaens@vodafoneziggo.com>
2026-05-12 16:21:57 +0200

    security session

--- lab/42-security-rest/src/main/java/config/RestSecurityConfig.java
+++ lab/42-security-rest/src/main/java/config/RestSecurityConfig.java
@@ -4,6 +4,8 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.core.userdetails.User;
@@ -17,6 +19,7 @@ import org.springframework.security.web.SecurityFilterChain;
 // - Add @EnableMethodSecurity annotation to this class
 
 @Configuration
+@EnableMethodSecurity
 public class RestSecurityConfig {
 
 	@Bean
@@ -33,7 +36,11 @@ public class RestSecurityConfig {
                 //   for all roles - "USER", "ADMIN", "SUPERADMIN"
         		// - Allow GET on the /authorities resource
                 //   for all roles - "USER", "ADMIN", "SUPERADMIN"
-
+                .requestMatchers(HttpMethod.GET, "/accounts/**").hasAnyRole("USER", "ADMIN", "SUPERADMIN")
+                .requestMatchers(HttpMethod.PUT, "/accounts/**").hasAnyRole("ADMIN", "SUPERADMIN")
+                .requestMatchers(HttpMethod.POST, "/accounts/**").hasAnyRole("ADMIN", "SUPERADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/accounts/**").hasAnyRole("SUPERADMIN")
+                .requestMatchers(HttpMethod.GET, "/authorities").hasAnyRole("USER", "ADMIN", "SUPERADMIN")
                 // Deny any request that doesn't match any authorization rule
                 .anyRequest().denyAll())
         .httpBasic(withDefaults())
@@ -55,9 +62,11 @@ public class RestSecurityConfig {
 		// - "superadmin"/"superadmin" with "USER", "ADMIN", and "SUPERADMIN" roles
 		// (Make sure to store the password in encoded form.)
     	// - pass all users in the InMemoryUserDetailsManager constructor
-		UserDetails user = User.withUsername("user").password(passwordEncoder.encode("user")).roles("USER").build();
+        UserDetails user = User.withUsername("user").password(passwordEncoder.encode("user")).roles("USER").build();
+        UserDetails admin = User.withUsername("admin").password(passwordEncoder.encode("admin")).roles("USER", "ADMIN").build();
+        UserDetails superadmin = User.withUsername("superadmin").password(passwordEncoder.encode("superadmin")).roles("USER", "ADMIN", "SUPERADMIN").build();
 
-		return new InMemoryUserDetailsManager(user /* Add new users comma-separated here */);
+       return new InMemoryUserDetailsManager(user, admin, superadmin);
 	}
     
     @Bean
--- lab/42-security-rest/src/test/java/accounts/services/AccountServiceMethodSecurityTest.java
+++ lab/42-security-rest/src/test/java/accounts/services/AccountServiceMethodSecurityTest.java
@@ -24,7 +24,6 @@ class AccountServiceMethodSecurityTest {
     private TestRestTemplate restTemplate;
 
     @Test
-    @Disabled
     void getAuthoritiesForUser_should_return_403_for_user() {
 
         ResponseEntity<String> responseEntity = restTemplate.withBasicAuth("user", "user")
@@ -34,7 +33,6 @@ class AccountServiceMethodSecurityTest {
     }
 
     @Test
-    @Disabled
     void getAuthoritiesForUser_should_return_authorities_for_admin() {
 
         String[] authorities = restTemplate.withBasicAuth("admin", "admin")
@@ -51,10 +49,14 @@ class AccountServiceMethodSecurityTest {
     //           three roles "ROLE_SUPERADMIN", "ROLE_ADMIN", and
     //           "ROLE_USER".
     @Test
-    public void getAuthoritiesForUser_should_return_authorities_for_superadmin() {
+    void getAuthoritiesForUser_should_return_authorities_for_superadmin() {
 
-
-
+        String[] authorities = restTemplate.withBasicAuth("superadmin", "superadmin")
+                                           .getForObject("/authorities?username=superadmin", String[].class);
+        assertThat(authorities.length).isEqualTo(3);
+        assertThat(authorities.toString().contains("ROLE_SUPERADMIN"));
+        assertThat(authorities.toString().contains("ROLE_ADMIN"));
+        assertThat(authorities.toString().contains("ROLE_USER"));
     }
 
 }
